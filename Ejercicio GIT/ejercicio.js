eb57c1271111aba4f455fcef34126be149c25e66
SaMu-mo <menesesorozcosamuel@gmail.com>
2025-09-20 12:48:03 -0500

    version recuperar Int, Float y html mejorada

--- Ejercicio GIT/ejercicio.js
+++
@@ -1,5 +0,0 @@
-saludar=function(){
-    let cmpImagenSaludo = document.getElementById("imgSaludo")
-    cmpImagenSaludo.src = "./Imagenes/GifGanar2.gif"
-    console.log("Holaaaaa")
-}
---
+++ Ejercicio GIT/saludo.js
@@ -0,0 +1,10 @@
+saludar = function(){
+    //recuperar el valor de la caja de texto txtNombre
+    let nombre = recuperarTexto("txtNombre");
+    //recuperar el valor de la caja de texto txtApellido
+    let apellido = recuperarTexto("txtApellido");
+
+    let edad = recuperarEntero("txtEdad");
+
+    let estatura = recuperarFloat("txtEstatura");
+}
